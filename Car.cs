3e21cff7fff3b8cbe2acf3e2a87c74558429f4eb
unknown <xudaatm@gmail.com>
2024-11-30 14:52:06 +0400

    added task

---
+++ Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    public class Car
+    {
+        private static int _idCounter = 1;  
+
+        public int Id { get; private set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public CarType Type { get; set; }
+
+        public Car(string brand, string model, CarType type)
+        {
+            Id = _idCounter++; 
+            Brand = brand;
+            Model = model;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Car ID: {Id}, Brand: {Brand}, Model: {Model}, Type: {Type}";
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Car car1 = new Car("Mercedes", "E200", CarType.Sedan);
+            Car car2 = new Car("BMW", "F10", CarType.Sedan);
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
+        }   
+       
+    }
+}
+
